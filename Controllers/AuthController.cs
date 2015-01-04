c2e506a18d3fa5b426ce88fd36076816e8027f79
Claus Bornich <clausb@gmail.com>
2015-01-04 13:28:17 +0100

    naive form where AuthController.cs returns username purely because matches with HTML form name

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        // default for  GET
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost] // explicitly directs POST here
+        public ActionResult Login(AuthLogin form)
+        {
+            return Content("Hi there " + form.Username);
         }
     }
 }
